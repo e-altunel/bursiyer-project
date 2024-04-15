625dbcc0eee554cedf29008888cb06c69b228f2b
Emirhan Altunel <101647178+e-altunel@users.noreply.github.com>
2024-04-15 13:07:34 +0300

    Add i18n support for Turkish translation

--- src/index.js
+++ src/index.js
@@ -2,9 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import i18next from "i18next";
+import { I18nextProvider } from "react-i18next";
+import global_tr from "./translations/tr/global.json";
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: "tr",
+  resources: {
+    tr: {
+      global: global_tr,
+    },
+  },
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18next}>
+        <App />
+    </I18nextProvider>
   </React.StrictMode>
 );
