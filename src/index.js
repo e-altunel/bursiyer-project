3303ce007a9eeb11f0932e97041ced0c3e619f1f
Emirhan Altunel <101647178+e-altunel@users.noreply.github.com>
2024-04-15 13:08:20 +0300

    Add BrowserRouter to App component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 import i18next from "i18next";
 import { I18nextProvider } from "react-i18next";
@@ -20,7 +21,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <I18nextProvider i18n={i18next}>
+      <BrowserRouter>
         <App />
+      </BrowserRouter>
     </I18nextProvider>
   </React.StrictMode>
 );
