ab59f21d511621e06f6777e4748a39d6959ada55
Emirhan Altunel <101647178+e-altunel@users.noreply.github.com>
2024-06-24 16:02:14 +0300

    Remove key element from Cbutton

--- src/pages/custom-comp/CButton.jsx
+++ src/pages/custom-comp/CButton.jsx
@@ -2,13 +2,22 @@ import { Button } from "react-bootstrap";
 import { useSelector } from "react-redux";
 
 export default function CButton(props) {
-  const { variant, ...other } = props;
+  const { variant, style, children, ...other } = props;
   const darkMode = useSelector((state) => state.uiSett.darkMode);
 
   return (
     <Button
       variant={variant ? variant : darkMode ? "outline-light" : "outline-dark"}
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "5px",
+        ...style,
+      }}
       {...other}
-    />
+    >
+      {children}
+    </Button>
   );
 }
